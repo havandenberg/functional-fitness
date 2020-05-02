e528568333f5f26156afdca7e1425105fa3b7b29
Halsey Vandenberg <halseyvandenberg@gmail.com>
2020-05-02 10:04:17 -0400

    play pause

--- src/routes/live.tsx
+++ src/routes/live.tsx
@@ -1,10 +1,14 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import styled from '@emotion/styled';
 import Parse from 'parse';
 import { find, propEq } from 'ramda';
 import { Redirect } from 'react-router-dom';
 import * as api from 'api';
 import nextImg from 'assets/images/next.svg';
+import pauseImg from 'assets/images/pause.svg';
+import playImg from 'assets/images/play.svg';
 import previousImg from 'assets/images/previous.svg';
+import resetImg from 'assets/images/reset.svg';
 import userImg from 'assets/images/user.svg';
 import Exercise from 'components/exercise';
 import { Sticky } from 'components/header';
@@ -17,27 +21,60 @@ import l from 'ui/layout';
 import th from 'ui/theme';
 import ty from 'ui/typography';
 
+const CountText = styled(ty.Text)({
+  'user-select': 'none',
+  fontSize: 30,
+  fontWeight: 'bold',
+});
+
+const TogglePlay = styled(l.Img)<{ duration: number; isActive: boolean }>(
+  {
+    height: th.sizes.xs,
+    position: 'absolute',
+    right: th.spacing.md,
+    top: '50%',
+    transform: 'translateY(-50%)',
+    transition: 'all 1s ease',
+  },
+  ({ duration, isActive }) => ({
+    opacity: duration % 2 === 0 || !isActive ? 1 : 0.4,
+  }),
+);
+
+const Reset = styled(l.Img)({
+  height: th.sizes.xs,
+  position: 'absolute',
+  left: th.spacing.md,
+  top: '50%',
+  transform: 'translateY(-50%)',
+});
+
+const TimeLabel = styled(ty.Label)({
+  position: 'absolute',
+  right: -24,
+  textAlign: 'center',
+  top: 0,
+  width: 20,
+});
+
 const Live = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
   const [{ user }] = useGlobalContext();
   const [{ liveId }] = useLiveQueryParams();
+
   const liveSessionsContent = api.fetchLiveSessionsContent();
   const liveSession = find(propEq('id', liveId || '0'), liveSessionsContent);
   const sessions = api.fetchSessions();
   const session = liveSession && find(propEq('id', liveSession.sessionId), sessions);
 
   const [activeLiveIndex] = api.useFetchActiveLiveSessionIndex();
-  const isLive = activeLiveIndex >= 0 && activeIndex === activeLiveIndex;
   const previousLiveIndex = usePrevious(activeLiveIndex);
+  const [activeIndex, setActiveIndex] = useState(activeLiveIndex >= 0 ? activeLiveIndex : 0);
+  const isLive = activeLiveIndex >= 0 && activeIndex === activeLiveIndex;
+
   const exercises = api.fetchExercises();
   const activeLiveExercise = liveSession && liveSession.exercises[activeIndex];
   const activeExercise = activeLiveExercise && find(propEq('id', activeLiveExercise.exerciseId), exercises);
-
-  useEffect(() => {
-    if (previousLiveIndex === activeIndex) {
-      setActiveIndex(activeLiveIndex);
-    }
-  }, [activeIndex, activeLiveIndex, previousLiveIndex]);
+  const isTime = activeLiveExercise && activeLiveExercise.type === 'time';
 
   const getDuration = useCallback((activeLiveExercise?: api.LiveExercise) => {
     let time = activeLiveExercise ? activeLiveExercise.count.split(':') : [];
@@ -50,40 +87,21 @@ const Live = () => {
     return duration;
   }, []);
 
-  const getTime = (duration: number) => {
-    const minutes = Math.floor(duration / 60);
-    const seconds = duration % 60;
-    return minutes ? `${minutes}:${seconds}` : seconds;
-  };
-
   const [duration, setDuration] = useState(getDuration(activeLiveExercise));
   const [isActive, setIsActive] = useState(false);
 
-  const reset = useCallback(() => {
-    setDuration(getDuration(activeLiveExercise));
-    setIsActive(false);
-  }, [activeLiveExercise, getDuration]);
+  const getTime = (duration: number) => {
+    const minutes = Math.floor(duration / 60);
+    const seconds = duration % 60;
+    return minutes ? `${minutes}${seconds % 2 === 0 || !isActive ? ':' : ' '}${seconds || '00'}` : `${seconds}`;
+  };
 
-  useEffect(() => {
-    if (activeLiveExercise && activeLiveExercise.type === 'time') {
-      setDuration(getDuration(activeLiveExercise));
-      setIsActive(true);
-    } else {
-      reset();
+  const decrement = () => {
+    const nextIndex = activeIndex - 1;
+    if (nextIndex >= 0) {
+      setActiveIndex(nextIndex);
     }
-  }, [activeLiveExercise, getDuration, reset]);
-
-  useEffect(() => {
-    let interval: any = null;
-    if (isActive) {
-      interval = setInterval(() => {
-        setDuration((duration) => duration - 1);
-      }, 1000);
-    } else if (!isActive && duration !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, duration]);
+  };
 
   const increment = useCallback(async () => {
     const nextIndex = activeIndex + 1;
@@ -100,12 +118,46 @@ const Live = () => {
     }
   }, [activeIndex, exercises.length, liveSession, user]);
 
-  const decrement = () => {
-    const nextIndex = activeIndex - 1;
-    if (nextIndex >= 0) {
-      setActiveIndex(nextIndex);
+  const reset = useCallback(() => {
+    setDuration(getDuration(activeLiveExercise));
+    setIsActive(false);
+  }, [activeLiveExercise, getDuration]);
+
+  const toggleActive = useCallback(() => {
+    setIsActive(!isActive);
+  }, [isActive]);
+
+  useEffect(() => {
+    if (previousLiveIndex === activeIndex || (previousLiveIndex === -1 && activeLiveIndex >= 0)) {
+      setActiveIndex(activeLiveIndex);
     }
-  };
+  }, [activeIndex, activeLiveIndex, previousLiveIndex]);
+
+  useEffect(() => {
+    if (activeLiveExercise && isTime) {
+      setDuration(getDuration(activeLiveExercise));
+      setIsActive(true);
+    } else {
+      reset();
+    }
+  }, [activeLiveExercise, getDuration, isTime, reset]);
+
+  useEffect(() => {
+    let interval: any = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        if (duration === 0) {
+          clearInterval(interval);
+          setIsActive(false);
+        } else {
+          setDuration((duration) => duration - 1);
+        }
+      }, 1000);
+    } else if (!isActive && duration !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, duration]);
 
   if (!activeExercise || !session || !activeLiveExercise || !liveSession) {
     return <Redirect to="/" />;
@@ -116,7 +168,7 @@ const Live = () => {
       <Sticky>
         <l.AreaLink to={`/sessions/${liveSession.sessionId}`}>
           <l.Centered pb={th.spacing.sm} pt={th.spacing.md} position="relative">
-            <ty.Label>Session</ty.Label>
+            <ty.Label>View Session</ty.Label>
             <ty.Text center>{session.name}</ty.Text>
             {user && (
               <l.Div position="absolute" right={56} top={12}>
@@ -152,11 +204,34 @@ const Live = () => {
               {activeLiveExercise.setCount}
             </ty.Text>
           </l.Centered>
-          <l.Centered flex={1} height={85}>
-            <ty.Label>{activeLiveExercise.type}</ty.Label>
-            <ty.Text bold fontSize="30px">
-              {getTime(duration) || activeLiveExercise.count}
-            </ty.Text>
+          <l.Centered
+            flex={1}
+            height={85}
+            position="relative"
+            onClick={isTime ? toggleActive : undefined}
+            onDoubleClick={isTime ? reset : undefined}
+          >
+            <l.Div position="relative">
+              <ty.Label>{activeLiveExercise.type}</ty.Label>
+              {isTime && <TimeLabel>({duration < 60 ? 's' : 'min'})</TimeLabel>}
+            </l.Div>
+            <CountText>{getTime(duration) || activeLiveExercise.count}</CountText>
+            {isTime && duration > 0 && (
+              <TogglePlay
+                duration={duration}
+                isActive={isActive}
+                src={isActive || getDuration(activeLiveExercise) === duration ? playImg : pauseImg}
+              />
+            )}
+            {isTime && getDuration(activeLiveExercise) !== duration && (!isActive || duration === 0) && (
+              <Reset
+                onClick={(e: React.ChangeEvent) => {
+                  e.stopPropagation();
+                  reset();
+                }}
+                src={resetImg}
+              />
+            )}
           </l.Centered>
         </l.Flex>
       </Sticky>
